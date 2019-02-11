5198357eb4c84088907f92d39821c1be1ccd3566
AidinFerdowsi <aidinferdowsi@gmail.com>
2019-02-11 02:09:13 -0500

    V 2.0.0

--- cartpoleQlearning.py
+++ cartpoleQlearning.py
@@ -6,6 +6,8 @@ Created on Fri Feb  8 19:15:28 2019
 """
 
 import numpy as np
+import gym
+import matplotlib.pyplot as plt
 
 
 def intStates(featureArr):
@@ -33,4 +35,75 @@ class FeatureTransformer:
                 makeBin(pAng, self.poleAngle),
                 makeBin(pVel, self.poleVelocity)])
     
+
+
+class Environment:
+    def __init__(self,env,FeatureTransformer):
+        self.env = env
+        self.FT = FeatureTransformer
+        self.alpha = 10e-3
+        
+        numState = 10**4
+        numActions = env.action_space.n
+        
+        self.Q = np.random.uniform(low=-1,high = 1,size = (numState,numActions))
+      
+    def stateActions(self,s):
+        x = self.FT.transfomer(s) 
+        return self.Q[x]
     
+    def update(self,s,a,G):
+        x = self.FT.transfomer(s)
+        self.Q[x,a] = self.alpha * (G - self.Q[x,a])
+        
+    def epsilonGreedy(self,s,eps):
+        if np.random.random() < eps:
+            return self.env.action_space.sample()
+        else:
+            x = self.FT.transfomer(s)
+            return np.argmax(self.Q[x])
+    
+    
+def playEpisode(Env,eps,gamma):
+    observation = env.reset()
+    done = False
+    totalReward = 0
+    iterations = 0
+    
+    while not done and iterations < 10000:
+        action = Env.epsilonGreedy(observation,eps)
+        previousObservation = observation
+        observation, reward, done, info = env.step(action)
+        
+        totalReward += reward
+        
+        if done and iterations < 199:
+            reward = -100
+         
+        G = reward + gamma*np.max(Env.stateActions(observation))
+        Env.update(previousObservation,action,G)
+        
+        iterations += 1
+    
+    return totalReward
+
+
+
+if __name__ == '__main__':
+    env = gym.make('CartPole-v0')
+    ft = FeatureTransformer()
+    Env = Environment(env,ft)
+    gamma = 0.9
+    
+    
+    N = 10000
+    totalReward = np.zeros(N)
+    
+    for n in range(N):
+        eps = 1.0/np.sqrt(n+1)
+        
+        totalReward[n] = playEpisode(Env,eps,gamma)
+        if n % 100 ==0:
+            print("Episode",n,"Total reward:", totalReward[n],"eps:",eps)
+    
+    plt.plot(totalReward)

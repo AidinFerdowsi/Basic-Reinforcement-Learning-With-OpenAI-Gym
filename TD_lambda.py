7bb223e67f311919bb9126b452fea4d594c0702d
AidinFerdowsi <aidinferdowsi@gmail.com>
2019-02-25 02:02:11 -0500

    V 6.0.1

--- TD_lambda.py
+++ TD_lambda.py
@@ -10,12 +10,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 import mountainClimbRBF 
-from mountainClimbRBF import FeatureTransformer, Model, plotAvgReward
+from mountainClimbRBF import FeatureTransformer, plotAvgReward
 
 
 
 
-class FitEligibility:
+class ModelEligibility:
     def __init__(self, D):
         self.w = np.random.rand(D)/np.sqrt(D)
         
@@ -27,3 +27,43 @@ class FitEligibility:
     def predict(self,X):
         X = np.array(X)
         return X.dot(self.w)
+    
+    
+
+
+class Model:
+    def __init__(self,env,feature_transformer):
+        self.env = env
+        self.ft = feature_transformer
+        self.models = []
+        
+        
+        D = feature_transformer.dimensions
+        self.eligibilities = np.zeros((env.action_space.n,D))
+        
+        for i in range(env.action_space.n):
+            self.models.append(ModelEligibility(D))
+            
+            
+    def evaluateState (self,s):
+        X = self.ft.transform([s])
+        assert(len(X.shape)==2)
+        return np.array([m.predict(X)[0] for m in self.models])
+    
+    
+    
+    def updateValue(self,s,a,G,gamma,lambda_):
+        X = self.ft.transform([s])
+        assert(len(X.shape)==2)
+        self.eligibilities *=gamma*lambda_
+        self.eligibilities[a] +=X[0]
+        self.models[a].partial_fit(X[0],G,self.eligibilities)
+        
+        
+    def takeAction(self,s,eps):
+        if np.random() <eps:
+            return self.env.action_space.sample()
+        else:
+            return np.argmax(self.predict(s))
+        
+        
